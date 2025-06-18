d0bae1462547f23e2ababf17ca1273b4149cd728
playbooks <eric@erichubbell.com>
2025-06-18 15:57:54 -0600

    package updates

--- packages/wrappers/scripts/packages.sh
+++
@@ -1,5 +0,0 @@
-npm i @ehubbell/html@latest
-npm i @playbooks/scrollspy@latest
-npm i @playbooks/confits@latest --save-dev
-
-npx yalc remove --all
--- packages/wrappers/src/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from 'src/animation';
-export * from 'src/fade';
-export * from 'src/scrollspy';
-export * from 'src/skeleton';
-export * from 'src/waypoint';
