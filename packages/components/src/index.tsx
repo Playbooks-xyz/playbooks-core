51d1898f79b7738dcaaeb29cfe0dcce52e7cc8c0
playbooks <eric@erichubbell.com>
2025-06-18 15:55:12 -0600

    component updates

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -2,6 +2,7 @@ export * from 'src/animation';
 export * from 'src/currency-input';
 export * from 'src/fade';
 export * from 'src/font-awesome';
+export * from 'src/google-autocomplete';
 export * from 'src/masked-input';
 export * from 'src/phone-input';
 export * from 'src/scrollspy';
