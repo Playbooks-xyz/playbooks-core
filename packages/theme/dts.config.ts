eb3fdb73745bfa6d4a294f1499cf73f292afaa41
playbooks <eric@erichubbell.com>
2025-06-19 08:21:13 -0600

    entry update

--- packages/theme/dts.config.ts
+++ packages/theme/dts.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
 	compilationOptions: { preferredConfigPath: './tsconfig.json' },
 	entries: ['index'].map(fileName => ({
-		filePath: `./src/${fileName}.ts`,
+		filePath: `./src/${fileName}.tsx`,
 		outFile: `./dist/${fileName}.d.ts`,
 		noCheck: false,
 	})),
