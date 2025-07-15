76ee200bf23d20fa932f6b213b5139066d429360
playbooks <eric@erichubbell.com>
2025-07-15 15:58:22 -0600

    package update

--- packages/theme/src/prefooters.tsx
+++ packages/theme/src/prefooters.tsx
@@ -11,12 +11,12 @@ export const prefooterBody = () => ({
 export const prefooterPretitle = () => ({});
 
 export const prefooterTitle = () => ({
-	color: 'text-white',
+	color: 'text-gray-700 dark:text-white',
 	fontWeight: 'font-bold',
 });
 
 export const prefooterSubtitle = () => ({
-	color: 'text-white',
+	color: 'text-gray-700 dark:text-white',
 	fontFamily: 'font-secondary',
 	fontSize: 'font-base',
 	fontWeight: 'font-light',
